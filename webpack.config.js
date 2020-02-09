d58ed8810d6e8fa8339b4337bf1f09b51099b72c
Vijay Kumar M <vijaykumar.mylapalli@outlook.com>
2020-02-09 23:41:28 +0530

    netlfiy redirect file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './app/index.js',
@@ -21,7 +22,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
-        })
+        }),
+        new CopyPlugin([
+            {from: '_redirects'}
+        ])
     ],
 
     devServer : {
